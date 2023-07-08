ae8899efd558350149c16d3671c560ed53da45ef
elenaskovo <elenaskovorodko@Elenas-Air.lan>
2023-07-08 11:29:16 -0400

    git commit test

--- src/test/GitTest.java
+++ src/test/GitTest.java
@@ -3,5 +3,8 @@ package test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("My Git Commit");
+        System.out.println("My Git Commit");
+        System.out.println("My Git Commit");
+
     }
 }
