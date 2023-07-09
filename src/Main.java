53e47b4e51c4e20f1c99909703c751ce27929f29
elenaskovo <elenaskovorodko@Elenas-Air.lan>
2023-07-09 01:25:00 -0400

    first push from the SolarSystemBatch16.iml

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        solarSystemBasic Omnax= new solarSystemBasic();
+        Omnax.Moon="2";
+        Omnax.Sun="1";
+        Omnax.Planets="2";
+        Omnax.Star="2";
+        System.out.println("Hello world!");
+        FeatureSun Xeon= new FeatureSun();
+        Xeon.Name="treo";
+        Xeon.size="-32432";
+    }
+}
